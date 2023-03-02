1c47103dfe35de0fa357d6aecd429978ea15b9ad
Aniq Javed <2aniqqjavedd4493@gmail.com>
2023-03-03 04:07:23 +0500

    Add User Controller for GetUser Route

--- controllers/user.go
+++ controllers/user.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/API-Golang/models"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p *httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectHex(id)
+
+	u := models.User{}
+
+	if err := uc.Session.DB("mongo.golang").C("users").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOk)
+	fmt.Fprintf(w, "%s\n", uj)
 }
