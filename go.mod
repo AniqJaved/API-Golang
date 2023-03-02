d208bfef92bc9d63774c54343a3c24d8e23ccc79
Aniq Javed <2aniqqjavedd4493@gmail.com>
2023-03-03 03:43:22 +0500

    Add Main, Model for User

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/API-Golang
 go 1.20
 
 require (
+	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+
+	"github.com/AniqJaved/API-Golang/controllers"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb+srv://AniqJaved:aniqjaved@cluster0.f8uqrsj.mongodb.net/?retryWrites=true&w=majority")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
